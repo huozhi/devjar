a4a655da2e935f56eba47011b999948391a689b0
Shu Ding <g@shud.in>
2022-04-23 21:43:42 +0200

    support for multiple files and recursive dependencies

--- docs/pages/index.js
+++ docs/pages/index.js
@@ -15,150 +15,195 @@ if (typeof window !== 'undefined') {
   window.React = React
 }
 
+class ErrorBoundary extends React.Component {
+  state = {
+    error: null,
+  }
+  componentDidCatch(error) {
+    this.setState({ error })
+  }
+  render() {
+    if (this.state.error) {
+      return <div>{this.state.error.message}</div>
+    }
+    return this.props.children
+  }
+}
+
 export default function Page() {
   const portalRef = useRef(null)
   const reactRootRef = useRef(null)
-  const [code, setCode] = useState(defaultText)
+
+  const [code, setCode] = useState({
+    'index.js': defaultText,
+  })
+  const [activeFile, setActiveFile] = useState('index.js')
+
   const { mod, error, load } = useDynamicModule()
 
-  useEffect(() => {
-    load(code)
-  }, [])
+  function renderModule() {
+    if (!mod.current) return
+    if (!mod.current.default) return
+    if (!reactRootRef.current) {
+      reactRootRef.current = ReactDOM.createRoot(portalRef.current)
+    }
+    const Component = mod.current.default
+    reactRootRef.current.render(
+      <ErrorBoundary>
+        <Component />
+      </ErrorBoundary>
+    )
+  }
 
   useEffect(() => {
-    if (error) {
-      console.error(error)
-    } else if (mod) {
-      window.__MOD__ = mod
-      const Component = mod.default
-      const element = <Component />
-      if (!reactRootRef.current) {
-        reactRootRef.current = ReactDOM.createRoot(portalRef.current)
-      }
-      reactRootRef.current.render(element)
-    }
-  }, [mod, error])
+    load(code).then(renderModule)
+  }, [code])
 
   return (
     <div>
       <style jsx global>{`
-      * {
-        box-sizing: border-box;
-      }
-      html {
-        font-family: "Inter",-apple-system,BlinkMacSystemFont,"Segoe UI","Roboto","Oxygen","Ubuntu","Cantarell","Fira Sans","Droid Sans","Helvetica Neue",sans-serif
-      }
-      body {
-        max-width: 690px;
-        margin: auto;
-        padding: 40px 10px 40px;
-      }
-      .sh__class {
-        color: #2d5e9d;
-      }
-      .sh__identifier {
-        color: #354150;
-      }
-      .sh__sign {
-        color: #8996a3;
-      }
-      .sh__string {
-        color: #00a99a;
-      }
-      .sh__keyword {
-        color: #f47067;
-      }
-      .sh__comment {
-        color: #a19595;
-      }
-      .sh__jsxliterals {
-        color: #6266d1;
-      }
-      .sh__line::before {
-        content: attr(data-line-number);
-        width: 24px;
-        display: inline-block;
-        margin-right: 20px;
-        text-align: right;
-        color: #a4a4a4;
-      }
-      .editor {
-        position: relative;
-        min-height: 100px;
-        display: flex;
-      }
-      .absolute-full {
-        position: absolute;
-        left: 0;
-        right: 0;
-        top: 0;
-        bottom: 0;
-      }
-      .pad {
-        overflow-wrap: break-word;
-        display: inline-block;
-        padding: 16px 12px;
-        background-color: #f6f6f6;
-        border: none;
-        border-radius: 12px;
-        font-family: Consolas, Monaco, monospace;
-        font-size: 16px;
-        line-height: 1.25em;
-        caret-color: #333;
-      }
-      .block {
-        display: block;
-        position: relative;
-      }
-      .pre {
-        margin: 0;
-        flex: 1 0;
-        white-space: pre-wrap;
-      }
-      .pre code {
-        width: 100%;
-        min-height: 100px;
-      }
-      .code-input {
-        resize: none;
-        display: block;
-        width: 100%;
-        background-color: transparent;
-        color: transparent;
-        padding-left: 54px;
-      }
-      .executor {
-        position: absolute;
-        right: 8px;
-        top: 8px;
-        border: none;
-        background: #333;
-        border-radius: 4px;
-        color: #fff;
-      }
+        * {
+          box-sizing: border-box;
+        }
+        html {
+          font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+            'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+            'Helvetica Neue', sans-serif;
+        }
+        body {
+          max-width: 690px;
+          margin: auto;
+          padding: 40px 10px 40px;
+        }
+        .sh__class {
+          color: #2d5e9d;
+        }
+        .sh__identifier {
+          color: #354150;
+        }
+        .sh__sign {
+          color: #8996a3;
+        }
+        .sh__string {
+          color: #00a99a;
+        }
+        .sh__keyword {
+          color: #f47067;
+        }
+        .sh__comment {
+          color: #a19595;
+        }
+        .sh__jsxliterals {
+          color: #6266d1;
+        }
+        .sh__line::before {
+          content: attr(data-line-number);
+          width: 24px;
+          display: inline-block;
+          margin-right: 20px;
+          text-align: right;
+          color: #a4a4a4;
+        }
+        .editor {
+          position: relative;
+          min-height: 100px;
+          display: flex;
+        }
+        .absolute-full {
+          position: absolute;
+          left: 0;
+          right: 0;
+          top: 0;
+          bottom: 0;
+        }
+        .pad {
+          overflow-wrap: break-word;
+          display: inline-block;
+          padding: 16px 12px;
+          background-color: #f6f6f6;
+          border: none;
+          border-radius: 12px;
+          font-family: Consolas, Monaco, monospace;
+          font-size: 16px;
+          line-height: 1.25em;
+          caret-color: #333;
+        }
+        .block {
+          display: block;
+          position: relative;
+        }
+        .pre {
+          margin: 0;
+          flex: 1 0;
+          white-space: pre-wrap;
+        }
+        .pre code {
+          width: 100%;
+          min-height: 100px;
+        }
+        .code-input {
+          resize: none;
+          display: block;
+          width: 100%;
+          background-color: transparent;
+          color: transparent;
+          padding-left: 54px;
+        }
+        .executor {
+          position: absolute;
+          right: 8px;
+          top: 8px;
+          border: none;
+          background: #333;
+          border-radius: 4px;
+          color: #fff;
+        }
       `}</style>
       <div>
         <h3>Code</h3>
-        <CodeEditor
-          defaultCode={defaultText}
-          onChange={(code) => {
-            setCode(code)
+        {Object.keys(code).map((filename) => (
+          <button
+            key={filename}
+            disabled={filename === activeFile}
+            onClick={() => setActiveFile(filename)}
+          >
+            {filename}
+          </button>
+        ))}
+        <button
+          onClick={() => {
+            const newFilename = Object.keys(code).length + '.js'
+            setCode({
+              ...code,
+              [newFilename]: `export default function () {}`,
+            })
+            setActiveFile(newFilename)
           }}
         >
-          <button
-            className='executor'
-            onClick={async () => {
-              load(code)
-            }}
-          >
-            run
-          </button>
-        </CodeEditor>
+          +
+        </button>
+        <CodeEditor
+          key={activeFile}
+          content={code[activeFile]}
+          onChange={(updated) => {
+            setCode({
+              ...code,
+              [activeFile]: updated,
+            })
+          }}
+        />
       </div>
 
       <div>
         <h3>Preview</h3>
-        <div className='pad block' ref={portalRef} />
+        <ErrorBoundary>
+          <div className='pad block' ref={portalRef} />
+        </ErrorBoundary>
+      </div>
+
+      <div>
+        <h3>Error</h3>
+        <div className='pad block'>{error ? error.message : null}</div>
       </div>
     </div>
   )
