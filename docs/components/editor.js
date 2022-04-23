7079fae5a1ae78e1e5c5b0b7fc5550d1bb0f7d41
Shu Ding <g@shud.in>
2022-04-24 00:06:16 +0200

    Add support for multiple-file and recursive-dependency (#1)

--- docs/components/editor.js
+++ docs/components/editor.js
@@ -1,39 +1,40 @@
 import { highlight } from 'sugar-high'
 import { useEffect, useState } from 'react'
 
-
 export default function CodeEditor({
   children,
   onChange = () => {},
-  defaultCode = ''
+  content = '',
 }) {
-  const [text, setText] = useState(defaultCode)
-  const [output, setOutput] = useState(highlight(text))
+  const [output, setOutput] = useState(highlight(content))
 
   function update(code) {
     if (!code) return
     const highlighted = highlight(code)
-    setText(code)
     setOutput(highlighted)
     onChange(code)
   }
 
-  async function onEnterText(event) {
+  function onEnterText(event) {
     const code = event.target.value || ''
     update(code)
   }
 
   useEffect(() => {
-    update(text)
-  }, [])
+    update(content)
+  }, [content])
 
   return (
-    <div className="block">
-      <div className="editor">
-        <pre className="pre">
-          <code className="pad" dangerouslySetInnerHTML={{ __html: output }}></code>
+    <div className='block'>
+      <div className='editor'>
+        <pre className='pre'>
+          <code className='pad' dangerouslySetInnerHTML={{ __html: output }} />
         </pre>
-        <textarea className="pad absolute-full code-input" value={text} onChange={onEnterText}></textarea>
+        <textarea
+          className='pad absolute-full code-input'
+          value={content}
+          onChange={onEnterText}
+        ></textarea>
       </div>
       {children}
     </div>
