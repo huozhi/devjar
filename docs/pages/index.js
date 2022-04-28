68e987b4913b97fd27c34d9023e50dfa9971cc54
Jiachi Liu <inbox@huozhi.im>
2022-04-29 01:16:28 +0200

    enhance: inject script

--- docs/pages/index.js
+++ docs/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Editor } from 'codice'
 import { useDynamicModule } from 'devjar/react'
 
